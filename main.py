3be99016ee5049fd88cfe15490b44842df9c4ce1
Yuri <Yuribtr@users.noreply.github.com>
2020-09-22 23:57:23 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+class Track:
+    def __init__(self, name='', duration=0):
+        self.name = name
+        self.duration = duration
+
+    def show(self):
+        return f'<{self.name} - {self.duration}>'
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_duration(self, duration):
+        self.duration = duration
+
+
+class Album:
+    def __init__(self, name='', group=''):
+        self.name = name
+        self.group = group
+        self.tracks = []
+
+    def get_tracks(self):
+        result = []
+        for track in self.tracks:
+            result.append(track.show())
+        return result
+
+    def get_duration(self):
+        result = 0
+        for track in self.tracks:
+            result += track.duration
+        return result
+
+    def add_track(self, track):
+        if not isinstance(track, Track):
+            raise NotImplementedError('Can not add this object to track list')
+        self.tracks.append(track)
+
+
+albums = []
+album = Album('The Fat of the Land', 'The Prodigy')
+album.add_track(Track('Narayan', 9))
+album.add_track(Track('Climbatize', 6))
+album.add_track(Track('Breathe', 5))
+albums.append(album)
+
+album = Album('Опиум', 'Агата Кристи')
+album.add_track(Track('Сказочная тайга', 2))
+album.add_track(Track('Чёрная луна', 4))
+album.add_track(Track('Трансильвания', 4))
+albums.append(album)
+
+for album in albums:
+    print(f'Альбом "{album.name}" группы {album.group}:')
+    for track in enumerate(album.get_tracks(), 1):
+        print(f'{track[0]}. {track[1]}')
+    print(f'Общая длительность альбома: {album.get_duration()} минут\n')
